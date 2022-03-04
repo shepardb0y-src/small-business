3fa860e09d512fb888af473f4f1810616700f1d6
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-03-04 14:04:11 -0500

    created prices componet and created cards

---
+++ src/components/PricesCard.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+export const PricesContainer = styled.div`
+display:flex;
+justify-content:center;
+align:items:center;
+gap:2rem;
+`
+export const Card = styled.div`
+
+overflow: hidden;
+width:300px;
+padding: 3rem 0 2rem;
+border-radius: 0.5rem;
+color:grey;
+border:1px solid black;
+`;
+export const Content = styled.div`
+
+
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+`;
+export const Title= styled.div`
+font-size:1.25rem;
+`;
+
+export const PackagePrice = styled.div`
+font-size:3rem
+color:grey;
+`;
+
+const PricesCard = () => {
+    return (
+        <PricesContainer>
+            <Card>
+                <Content>
+                    <Title>Basic</Title>
+                    <PackagePrice>$50</PackagePrice>
+                </Content>
+            </Card>
+            <Card>
+                <Content>
+                    <Title>Basic</Title>
+                    <PackagePrice>$50</PackagePrice>
+                </Content>
+            </Card>
+            <Card>
+                <Content>
+                    <Title>Basic</Title>
+                    <PackagePrice>$50</PackagePrice>
+                </Content>
+            </Card>
+        </PricesContainer>
+    );
+}
+
+export default PricesCard;
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,8 +1,18 @@
 import React from "react";
+import styled from 'styled-components'
+import PricesCard from "../components/PricesCard";
+
+
+export const ServicesContainer = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+margin-top: 15%;
+`
 
 
 const Services = () => {
-  return <div>services</div>;
+  return <ServicesContainer><PricesCard/></ServicesContainer>;
 };
 
 export default Services;
