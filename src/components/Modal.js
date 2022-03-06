7ffe633159fe22934ed35d06e7a585e7b4bb81fc
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-03-06 12:55:35 -0500

    created modal base architecture

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import styled from 'styled-components'
 
-const Modals = () => {
+const ModalBackground= styled.div`
+display:flex;
+justify:content;
+align-items:center;
+background-color:grey;
+`
+const ModalContainer= styled.div`
+display:flex;
+justify:content;
+align-items:center;
+background-color:grey;
+flex-direction:column;
+`
+const Title= styled.div`
+background-color:blue;
+`
+const Body= styled.div`
+background-color:pink;
+`
+const Footer= styled.div`
+background-color:orange;
+`
+const Button= styled.button`
+background-color:red;
+`
+const Modal = () => {
     return (
-        <div>
-            Modal
-        </div>
+        <ModalBackground>
+        <ModalContainer>
+            <Button>X</Button>
+            <Title>Modal-title</Title>
+            <Body>MOdal-Body</Body>
+           <Footer> MOdal-footer</Footer>
+        </ModalContainer>
+        </ModalBackground>
     );
 }
 
-export default Modals;
+export default Modal;
--- src/components/PricesCard.js
+++ src/components/PricesCard.js
@@ -69,6 +69,7 @@ color:grey;
 `;
 
 
+
 const PricesCard = () => {
   return (
     <PricesContainer>
